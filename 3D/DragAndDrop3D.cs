a88401b8796a5e400d1e65574340ef8ea91e00a6
周张林 <zhouzhanglin@gmail.com>
2015-03-22 14:57:22 +0800

    no message

---
+++ 3D/DragAndDrop3D.cs
@@ -0,0 +1,263 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 拖放3D物体，使用两种方式，一种用系统OnMouseDown,OnMouseDrag,OnMouseUp事件，另一种用射线检测。注意：使用的是主相机(mainCamera)
+/// send Message:OnDragAndDropDown,OnDragAndDropMove,OnDragAndDropRelease
+/// author:zhouzhanglin
+/// 2015/3/12
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class DragAndDrop3D : MonoBehaviour {
+
+	public enum DragBackEffect
+	{
+		None,Immediately,TweenPosition,TweenScale
+	}
+
+	private Vector3 m_cachePosition ;
+	private Vector3 m_cacheScale;
+	private Vector3 m_dragOffset;
+	private Vector3 m_screenPosition;
+	private bool m_haveRigidbody;
+	private bool m_defaultIsKinematic;
+	private Transform m_trans;
+	private Collider m_collider;
+	private bool m_isDown;
+	private Vector3 m_currentPosition ;
+
+	[Tooltip("是否发送鼠标事件，OnDragAndDropDown,OnDragAndDropMove,OnDragAndDropRelease.")]
+	public bool isSendMouseEvent = false;
+
+	[Tooltip("是否使用射线检测.如果是，则设置rayCastMasks中的参数.")]
+	public bool isUseRaycast = false ;
+
+	[Tooltip("射线的检测距离，只用于射线检测时.")]
+	public float raycastDistance = 100f;
+
+	[Tooltip("射线检测的Layer")]
+	public LayerMask[] rayCastMasks ;
+
+	[Tooltip("在拖动时是否固定在拖动物的原点.")]
+	public bool isDragOriginPoint = false ;
+
+	[Tooltip("如果isDragOriginPoint为true,则可以设置拖动时的偏移值.")]
+	public Vector3 dragOffset ;
+
+	[Tooltip("拖动时的缓动参数.")]
+	public float dragMoveDamp = 0.5f;
+
+	[Tooltip("移动时在哪个面上移动，如果为null，则在拖动物的Z轴面移动.")]
+	public GameObject mousePickLayer = null;
+
+	[Tooltip("drop容器所在的层.")]
+	public LayerMask[] dropLayerMasks ;
+
+	[Tooltip("drop发生时发送的事件，drop和当前拖动对象都会发送.")]
+	public string dropMedthod = "OnDrop";
+
+	[Tooltip("如果没有检测到可drop的容器，是否返回原来的位置.")]
+	public bool isDropFailBack = true;
+
+	[Tooltip("返回原来位置时的效果.")]
+	public DragBackEffect dragBackEffect = DragBackEffect.None;
+
+	[Tooltip("返回原来位置时的速度.对TweenPosition和TweenScale有用.")]
+	public float backEffectSpeed = 10f;
+
+
+	#region MonoBehaviour内置方法.
+	void Start () {
+		m_trans = transform;
+		m_collider = GetComponent<Collider> ();
+		if (mousePickLayer) {
+			mousePickLayer.SetActive(false);
+		}
+	}
+	void Update () {
+		if (isUseRaycast && Input.touchCount < 2) {
+			if(Input.GetMouseButtonDown(0)){
+				if(!m_isDown){
+					int mask = GetLayerMask (rayCastMasks);
+					RaycastHit hit;
+					if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,raycastDistance,mask)){
+						if(hit.collider.gameObject==gameObject){
+							m_isDown=true;
+							OnMouseDownHandler();
+						}
+					}
+				}
+			}
+			else if(m_isDown && Input.GetMouseButtonUp(0)){
+				m_isDown = false;
+				OnMouseUpHandler();
+			}
+			else if(m_isDown){
+				OnMouseDragHandler();
+			}
+		}
+	}
+	void OnMouseDown(){
+		if (!isUseRaycast&&Input.touchCount < 2) {
+			OnMouseDownHandler();
+		}
+	}
+	void OnMouseDrag(){
+		if (!isUseRaycast&&Input.touchCount < 2) {
+			OnMouseDragHandler ();
+		}
+	}
+	void OnMouseUp(){
+		if (!isUseRaycast&&Input.touchCount < 2) {
+			OnMouseUpHandler();
+		}
+	}
+#endregion
+
+
+	private void OnMouseDownHandler(){
+		StopAllCoroutines ();
+		m_cachePosition = m_trans.position;
+		m_cacheScale = m_trans.localScale;
+		m_currentPosition = m_cachePosition;
+		m_dragOffset = Vector3.zero;
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		if (rb) {
+			m_haveRigidbody = true;
+			m_defaultIsKinematic = rb.isKinematic;
+			rb.isKinematic = true;
+		} else {
+			m_haveRigidbody = false;
+		}
+		m_screenPosition = Camera.main.WorldToScreenPoint(m_trans.position);
+		if (!isDragOriginPoint) {
+			m_dragOffset = m_trans.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, m_screenPosition.z));
+		}
+		if (isUseRaycast && mousePickLayer) {
+			mousePickLayer.SetActive (true);
+		}
+		if (isSendMouseEvent) {
+			gameObject.SendMessage("OnDragAndDropDown",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	private void OnMouseDragHandler(){
+		if (isUseRaycast && mousePickLayer) {
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,raycastDistance,1<<mousePickLayer.layer)){
+				if(hit.collider.gameObject==mousePickLayer){
+					m_currentPosition = hit.point;
+				}
+			}
+		} else {
+			Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_screenPosition.z);
+			m_currentPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) ;
+		}
+
+		if (!isDragOriginPoint) {
+			m_currentPosition += m_dragOffset;
+		} else {
+			m_currentPosition += dragOffset;
+		}
+		if (dragMoveDamp > 0f) {
+			m_trans.position = Vector3.Lerp(m_trans.position,m_currentPosition,dragMoveDamp);
+		} else {
+			m_trans.position = m_currentPosition;
+		}
+		if (isSendMouseEvent) {
+			gameObject.SendMessage("OnDragAndDropMove",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	private void OnMouseUpHandler(){
+		if (mousePickLayer) {
+			mousePickLayer.SetActive (false);
+		}
+		if (m_haveRigidbody) {
+			Rigidbody rb = GetComponent<Rigidbody> ();
+			if (rb) {
+				rb.isKinematic = m_defaultIsKinematic;
+			}
+		}
+		//check drop
+		int mask = GetLayerMask (dropLayerMasks);
+		RaycastHit hit;
+		if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,raycastDistance,mask)){
+			if(hit.collider.gameObject!=gameObject){ //Exclude myself
+				hit.collider.SendMessage(dropMedthod,SendMessageOptions.DontRequireReceiver);
+				gameObject.SendMessage(dropMedthod,gameObject,SendMessageOptions.DontRequireReceiver);
+			}else{
+				BackPosition();
+			}
+		}
+		else
+		{
+			BackPosition ();
+		}
+		if (isSendMouseEvent) {
+			gameObject.SendMessage("OnDragAndDropRelease",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	/// <summary>
+	/// 根据LayerMask数组来获取 射线检测的所有层.
+	/// </summary>
+	/// <returns>The layer mask.</returns>
+	/// <param name="masks">Masks.</param>
+	private int GetLayerMask(LayerMask[] masks){
+		int mask = Physics.AllLayers ;
+		if(masks!=null && masks.Length>0){
+			mask = masks[0].value ;
+			for(int i=1;i<masks.Length;i++){
+				mask = mask|masks[i].value;
+			}
+		}
+		return mask;
+	}
+
+
+	/// <summary>
+	/// 返回到原来的位置.
+	/// </summary>
+	public void BackPosition(){
+		if (isDropFailBack) {
+			switch(dragBackEffect){
+			case DragBackEffect.TweenPosition:
+				StartCoroutine("BackTween");
+				break;
+			case DragBackEffect.Immediately:
+				m_trans.position = m_cachePosition ;
+				m_collider.enabled = true;
+				break;
+			case DragBackEffect.TweenScale:
+				m_trans.position = m_cachePosition ;
+				m_trans.localScale = Vector3.zero;
+				StartCoroutine("ScaleTween");
+				break;
+			default :
+				m_collider.enabled = true;
+				break;
+			}
+		}
+	}
+	private IEnumerator BackTween(){
+		m_collider.enabled = false;//Prevent dragging
+		while (Vector3.Distance(m_trans.position,m_cachePosition)>0.01f) {
+			m_trans.position = Vector3.Lerp(m_trans.position,m_cachePosition,backEffectSpeed*Time.fixedDeltaTime);
+			yield return new WaitForFixedUpdate();
+		}
+		m_trans.position = m_cachePosition ;
+		m_collider.enabled = true;
+	}
+	private IEnumerator ScaleTween(){
+		m_collider.enabled = false;//Prevent dragging
+		while (Vector3.Distance(m_trans.localScale,m_cacheScale)>0.01f) {
+			m_trans.localScale = Vector3.Lerp(m_trans.localScale,m_cacheScale,backEffectSpeed*Time.fixedDeltaTime);
+			yield return new WaitForFixedUpdate();
+		}
+		m_trans.localScale = m_cacheScale ;
+		m_collider.enabled = true;
+	}
+
+}
---
+++ 3D/SpinWithMouse3D.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 旋转物体。注意：使用的是主相机(mainCamera)
+/// send Message:OnSpinWithMouseDown,OnSpinWithMouseMove,OnSpinWithMouseRelease,OnSpinWithMouseRotate
+/// author:zhouzhanglin
+/// 2015/3/13
+/// </summary>
+public class SpinWithMouse3D : MonoBehaviour {
+
+	public enum RotationAxis
+	{
+		DirX,DirY,DirZ
+	}
+	public enum DragDirection
+	{
+		LeftAndRight,UpAndDown,Left,Right,Up,Down
+	}
+
+	private Vector3 m_screenPos;
+	private Transform m_trans;
+	private float m_rotateToAngleDamp;
+
+	[Tooltip("旋转轴.")]
+	public RotationAxis rotationAxis = RotationAxis.DirZ;
+
+	[Tooltip("旋转的空间.")]
+	public Space rotationSpace=Space.Self;
+
+	[Tooltip("鼠标允许拖动的方向.鼠标在此方向上移动，物体才会旋转.")]
+	public DragDirection dragDir = DragDirection.UpAndDown;
+
+	[Tooltip("拖动时的旋转速度.")]
+	public float dragRotationSpeed = 2f;
+
+	#region MonoBehaviour内置
+	void Awake(){
+		m_trans = transform;
+	}
+
+	void OnMouseDown(){
+		StopAllCoroutines ();
+		m_screenPos = Camera.main.WorldToScreenPoint (Input.mousePosition);
+		gameObject.SendMessage("OnSpinWithMouseDown",SendMessageOptions.DontRequireReceiver);
+	}
+	void OnMouseDrag(){
+		Vector3 currentScreen = Camera.main.WorldToScreenPoint (Input.mousePosition);
+		if (dragDir == DragDirection.UpAndDown) {
+			Rotate ( m_screenPos.y - currentScreen.y);
+		} else if (dragDir == DragDirection.LeftAndRight) {
+			Rotate (currentScreen.z - m_screenPos.z);
+		}else if (dragDir == DragDirection.Left) {
+			if(currentScreen.z>m_screenPos.z)
+				Rotate (currentScreen.z - m_screenPos.z);
+		}else if (dragDir == DragDirection.Right) {
+			if(currentScreen.z<m_screenPos.z)
+				Rotate (currentScreen.z - m_screenPos.z);
+		}else if (dragDir == DragDirection.Up) {
+			if(currentScreen.y<m_screenPos.y)
+				Rotate ( m_screenPos.y - currentScreen.y);
+		}else if (dragDir == DragDirection.Down) {
+			if(currentScreen.y>m_screenPos.y)
+				Rotate ( m_screenPos.y - currentScreen.y);
+		}
+		m_screenPos = currentScreen;
+		gameObject.SendMessage("OnSpinWithMouseMove",SendMessageOptions.DontRequireReceiver);
+	}
+	void OnMouseUp(){
+		gameObject.SendMessage("OnSpinWithMouseRelease",SendMessageOptions.DontRequireReceiver);
+	}
+	#endregion
+
+
+
+	private void Rotate( float delta){
+		float change = delta*dragRotationSpeed*Time.deltaTime ;
+		if (rotationSpace == Space.Self) {
+			if (rotationAxis == RotationAxis.DirZ) {
+				m_trans.localRotation = Quaternion.Euler(0f, 0f ,change ) * m_trans.localRotation;
+			}else if (rotationAxis == RotationAxis.DirX) {
+				m_trans.localRotation = Quaternion.Euler(change, 0f ,0f ) * m_trans.localRotation;
+			}else if (rotationAxis == RotationAxis.DirY) {
+				m_trans.localRotation = Quaternion.Euler(0f, change, 0f ) * m_trans.localRotation;
+			}
+		} else {
+			if (rotationAxis == RotationAxis.DirZ) {
+				m_trans.rotation = Quaternion.Euler(0f, 0f ,change ) * m_trans.rotation;
+			}else if (rotationAxis == RotationAxis.DirX) {
+				m_trans.rotation = Quaternion.Euler(change, 0f ,0f ) * m_trans.rotation;
+			}else if (rotationAxis == RotationAxis.DirY) {
+				m_trans.rotation = Quaternion.Euler(0f, change, 0f ) * m_trans.rotation;
+			}
+		}
+		gameObject.SendMessage("OnSpinWithMouseRotate",SendMessageOptions.DontRequireReceiver);
+	}
+
+	/// <summary>
+	/// 旋转到一定的角度.
+	/// </summary>
+	/// <param name="angle">Angle.</param>
+	/// <param name="rotateToAngleDamp">旋转的动画缓冲值.</param>
+	public void RotationToAngle( float angle ,float rotateToAngleDamp=0.5f ){
+		this.m_rotateToAngleDamp = rotateToAngleDamp;
+		StartCoroutine ("RotationToAngleTween",angle);
+	}
+
+	private IEnumerator RotationToAngleTween( float angle ){
+		if (rotationSpace == Space.Self) {
+
+			if (rotationAxis == RotationAxis.DirZ) {
+				while(Mathf.Abs(m_trans.localEulerAngles.z-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.localEulerAngles.z,angle,m_rotateToAngleDamp);
+					Vector3 localRotation = m_trans.localEulerAngles;
+					localRotation.z = temp;
+					m_trans.localEulerAngles = localRotation;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.localEulerAngles;
+				rotation.z = angle;
+				m_trans.localEulerAngles = rotation;
+			}
+			else if (rotationAxis == RotationAxis.DirX){
+				while(Mathf.Abs(m_trans.localEulerAngles.x-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.localEulerAngles.x,angle,m_rotateToAngleDamp);
+					Vector3 localRotation = m_trans.localEulerAngles;
+					localRotation.x = temp;
+					m_trans.localEulerAngles = localRotation;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.localEulerAngles;
+				rotation.x = angle;
+				m_trans.localEulerAngles = rotation;
+			}
+			else if (rotationAxis == RotationAxis.DirY){
+				while(Mathf.Abs(m_trans.localEulerAngles.y-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.localEulerAngles.y,angle,m_rotateToAngleDamp);
+					Vector3 localRotation = m_trans.localEulerAngles;
+					localRotation.y = temp;
+					m_trans.localEulerAngles = localRotation;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.localEulerAngles;
+				rotation.y = angle;
+				m_trans.localEulerAngles = rotation;
+			}
+
+		}else {
+
+			if (rotationAxis == RotationAxis.DirZ) {
+				while(Mathf.Abs(m_trans.eulerAngles.z-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.eulerAngles.z,angle,m_rotateToAngleDamp);
+					Vector3 angles = m_trans.eulerAngles;
+					angles.z = temp;
+					m_trans.eulerAngles = angles;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.eulerAngles;
+				rotation.z = angle;
+				m_trans.eulerAngles = rotation;
+			}
+			else if (rotationAxis == RotationAxis.DirX){
+				while(Mathf.Abs(m_trans.eulerAngles.x-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.eulerAngles.x,angle,m_rotateToAngleDamp);
+					Vector3 angles = m_trans.eulerAngles;
+					angles.x = temp;
+					m_trans.eulerAngles = angles;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.eulerAngles;
+				rotation.x = angle;
+				m_trans.eulerAngles = rotation;
+			}
+			else if (rotationAxis == RotationAxis.DirY){
+				while(Mathf.Abs(m_trans.eulerAngles.y-angle)>0.01f){
+					float temp = Mathf.Lerp(m_trans.eulerAngles.y,angle,m_rotateToAngleDamp);
+					Vector3 localRotation = m_trans.eulerAngles;
+					localRotation.y = temp;
+					m_trans.eulerAngles = localRotation;
+					yield return new WaitForFixedUpdate();
+				}
+				Vector3 rotation = m_trans.eulerAngles;
+				rotation.y = angle;
+				m_trans.eulerAngles = rotation;
+			}
+
+		}
+			
+
+
+	}
+}
