fe9f9c466c44e0c50310e77fc12aabd2986a6533
周张林 <zhouzhanglin@gmail.com>
2015-03-22 15:13:10 +0800

    no message

---
+++ Assets/UGUI/Util/EventTriggerListener.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class EventTriggerListener : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler,
+IPointerExitHandler, IPointerUpHandler, ISelectHandler, IUpdateSelectedHandler
+{
+    public delegate void VoidDelegate1(GameObject go, PointerEventData eventData);
+    public delegate void VoidDelegate2(GameObject go, BaseEventData eventData);
+    public VoidDelegate1 onClick;
+    public VoidDelegate1 onDown;
+    public VoidDelegate1 onEnter;
+    public VoidDelegate1 onExit;
+    public VoidDelegate1 onUp;
+    public VoidDelegate2 onSelect;
+    public VoidDelegate2 onUpdateSelect;
+
+    static public EventTriggerListener Get(GameObject go)
+    {
+        EventTriggerListener listener = go.GetComponent<EventTriggerListener>();
+        if (listener == null) listener = go.AddComponent<EventTriggerListener>();
+        return listener;
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (onClick != null) onClick(gameObject,eventData);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (onDown != null) onDown(gameObject, eventData);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (onEnter != null) onEnter(gameObject, eventData);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (onExit != null) onExit(gameObject, eventData);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (onUp != null) onUp(gameObject, eventData);
+    }
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (onSelect != null) onSelect(gameObject,eventData);
+    }
+    public void OnUpdateSelected(BaseEventData eventData)
+    {
+        if (onUpdateSelect != null) onUpdateSelect(gameObject, eventData);
+    }
+}
---
+++ Assets/UGUI/_Sample/Script/TouchIdTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TouchIdTest : MonoBehaviour {
+
+    public Image img1, img2;
+    public Text t1, t2;
+	// Use this for initialization
+	void Start () {
+        EventTriggerListener.Get(img1.gameObject).onDown += OnImg1Click;
+        EventTriggerListener.Get(img2.gameObject).onDown += OnImg2Click;
+	}
+
+    private void OnImg1Click(GameObject go, UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        t1.text = eventData.pointerId+"";
+    }
+
+    private void OnImg2Click(GameObject go, UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        t2.text = eventData.pointerId + "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
