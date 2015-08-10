176e13d575caaebf5e18d2874c527d649fc08693
周张林 <zhouzhanglin@gmail.com>
2015-08-10 22:46:35 +0800

    Mobile Paint

---
+++ Assets/MobilePaint/Editor/PainterEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Painter))]
+public class PainterEditor : Editor {
+
+	public override void OnInspectorGUI(){
+
+		Painter source = (Painter)target;
+		EditorGUILayout.BeginVertical();
+
+		source.isAutoInit = EditorGUILayout.Toggle (new GUIContent ("Is Auto Init"), source.isAutoInit);
+
+		source.lerpDamp = EditorGUILayout.FloatField(new GUIContent("Brush Lerp"),source.lerpDamp);
+		source.isEraser = EditorGUILayout.Toggle(new GUIContent("Is Eraser"),source.isEraser);
+
+		source.brushType = (Painter.BrushType)EditorGUILayout.EnumPopup(new GUIContent("Brush Type"), source.brushType);
+
+		if (source.brushType == Painter.BrushType.CustomBrush) {
+			source.pen = (Texture2D)EditorGUILayout.ObjectField (new GUIContent ("Brush"), source.pen, typeof(Texture2D), true);
+			source.penAlphaEnable = EditorGUILayout.Toggle(new GUIContent("Pen Alpha Support"),source.penAlphaEnable);
+		} else {
+			source.brushSize = EditorGUILayout.IntField(new GUIContent("Brush Size"),source.brushSize);
+		}
+
+		source.paintType = (Painter.PaintType)EditorGUILayout.EnumPopup(new GUIContent("Paint Type"), source.paintType);
+		if (source.paintType == Painter.PaintType.Scribble) {
+			source.source = (Texture2D)EditorGUILayout.ObjectField (new GUIContent ("Source Texture"), source.source, typeof(Texture2D), true);
+		} else if (source.paintType == Painter.PaintType.DrawLine) {
+			source.paintColor = EditorGUILayout.ColorField(new GUIContent("Paint Color"),source.paintColor);
+		}
+
+		EditorGUILayout.EndVertical ();
+	}
+}
+
---
+++ Assets/MobilePaint/Sampler/Script/GUIController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+
+    public Texture2D drawImage;
+
+    private Painter _paint;
+    private bool _selectedEraser;
+    private bool _selectedDrawImg;
+
+	// Use this for initialization
+	void Start () {
+		_paint = GetComponent<Painter> ();
+		Application.targetFrameRate = 60;
+	}
+	
+	void OnGUI()
+    {
+        _selectedDrawImg = GUI.Toggle(new Rect(0, 0, 100, 30), _selectedDrawImg, "Is Draw Img");
+        if (!_selectedDrawImg)
+        {
+            _selectedEraser = GUI.Toggle(new Rect(0, 30, 100, 30), _selectedEraser, "Is Eraser");
+            _paint.isEraser = _selectedEraser;
+        }
+	}
+
+ 
+    //test draw img
+    void Update()
+    {
+        if (_selectedDrawImg)
+        {
+            if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Vector2 point = hit.textureCoord;
+                    _paint.DrawTexture(drawImage, point.x, point.y);
+                }
+            }
+        }
+    }
+
+}
