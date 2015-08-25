74639c6bda8346a310e23ab662f04fe82d5dd45a
zhouzhanglin <zhouzhanglin@gmail.com>
2015-08-25 17:00:34 +0800

    Camer3DAdjustWidth

---
+++ Assets/Utils/Camera3DAdjust.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class Camera3DAdjust : MonoBehaviour {
+
+    public float designWidth, designHeight;
+
+    void Awake()
+    {
+        Camera camera = Camera.main;
+        if (designHeight != 0f && Screen.height != 0f)
+        {
+            float radioDesign = designWidth / designHeight;
+            float radioScreen = (float)Screen.width / Screen.height;
+            if (radioDesign < radioScreen)
+            {
+                camera.fieldOfView *= radioDesign / radioScreen;
+                camera.fieldOfView = (float)System.Math.Round(camera.fieldOfView+0.05f, 2);
+            }
+        }
+    }
+
+}
