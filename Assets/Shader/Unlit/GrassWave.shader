1b08871138560ea53314189e3bca3d7567a31762
周张林 <zhouzhanglin@gmail.com>
2015-10-01 13:56:29 +0800

    Grass Wave

---
+++ Assets/Shader/Unlit/GrassWave.shader
@@ -0,0 +1,93 @@
+﻿Shader "ZZL/Unlit/Grass Wave"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_WaveDistance("Wave Distance",Range(0,1))=0.25
+		_WindSpeed("Wind Speed",Range(0,2))=0.4
+//		_RockPower("RockPower",float)=1
+		_Cutoff("alpha CutOff",Range(0,1)) = 0.5
+	}
+	SubShader
+	{
+		Tags {"Queue"="AlphaTest" "IgnoreProjector"="True" "RenderType"="Transparent"}
+		
+		ZWrite Off
+		Cull Off
+		Blend SrcAlpha OneMinusSrcAlpha 
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			half _WaveDistance;
+			fixed _WindSpeed;
+//			float _RockPower;
+			fixed _Cutoff;
+			
+			float4 GrassRock(float4 pos, half2 uv ,float dir)
+			{
+				if (uv.y > 0.99){
+
+					float windx = 0;
+					float windz = 0;
+					if ( dir >0 ){
+						windx = sin(_Time.y * _WindSpeed)*_WaveDistance;
+					}else{
+						windz =  cos(_Time.y * _WindSpeed)*_WaveDistance;
+					} 
+//					if (uv1.x >0) {
+//						float percentage = 1- uv1.x/(_totalTime);
+//						if (dir < 0) {
+//							windx += sin(uv1.x * _RockSpeed) * _RockPower * percentage ;
+//							windz += cos(uv1.x * _RockSpeed) * _RockPower * percentage ;
+//						}else{
+//							windx -= sin(uv1.x * _RockSpeed) * _RockPower * percentage ;
+//							windz -= cos(uv1.x * _RockSpeed) * _RockPower * percentage ;
+//						}
+//					} 
+					pos.x += windx;
+					pos.z += windz;
+				}
+				return pos;
+			}
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				half2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				half2 uv;
+				float4 pos : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.pos = GrassRock(o.pos, v.uv ,1);
+//				o.uv = v.uv;
+				o.uv = TRANSFORM_TEX(v.uv,_MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				clip(col.a-_Cutoff);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
--- Assets/UGUI/UnityEngine.UI/Gradient.cs
+++ Assets/UGUI/UnityEngine.UI/Gradient.cs
@@ -14,20 +14,23 @@ namespace UnityEngine.UI
 		[SerializeField]
 		private Color32 bottomColor = Color.black;
 
-		public override void ModifyMesh (Mesh mesh)
-		{
+#if UNITY_5_2_1
+		public override void ModifyMesh (VertexHelper vh){
 			if (!IsActive()) {
 				return;
 			}
-
-			int count = mesh.vertexCount;
-			if(count>0){
-		
-				float bottomY = mesh.vertices[0].y;
-				float topY = mesh.vertices[0].y;
-				
-				for (int i = 1; i < count; i++) {
-					float y = mesh.vertices[i].y;
+			float bottomY = 0;
+			float topY = 0;
+			for(int i=0;i<vh.currentVertCount;i++){
+				UIVertex v=new UIVertex();
+				vh.PopulateUIVertex(ref v,i);
+				if(i==0){
+					bottomY =v.position.y;
+					topY = v.position.y;
+				}
+				else
+				{
+					float y = v.position.y;
 					if (y > topY) {
 						topY = y;
 					}
@@ -35,16 +38,52 @@ namespace UnityEngine.UI
 						bottomY = y;
 					}
 				}
-				
-				float uiElementHeight = topY - bottomY;
-
-				Color32[] cs = mesh.colors32;
-				for (int i = 0; i < count; i++) {
-					Vector3 uiVertex = mesh.vertices[i];
-					cs[i] = Color32.Lerp(bottomColor, topColor, (uiVertex.y - bottomY) / uiElementHeight);
-				}
-				mesh.colors32 = cs ;
+			}
+			float uiElementHeight = topY - bottomY;
+			for(int i=0;i<vh.currentVertCount;i++){
+				UIVertex v=new UIVertex();
+				vh.PopulateUIVertex(ref v,i);
+				v.color = Color32.Lerp(bottomColor, topColor, (v.position.y - bottomY) / uiElementHeight);
+				vh.SetUIVertex(v,i);
 			}
 		}
 	}
+#elif UNITY_5_2
+
+	
+	public override void ModifyMesh (Mesh mesh)
+	{
+		if (!IsActive()) {
+			return;
+		}
+		
+		int count = mesh.vertexCount;
+		if(count>0){
+			
+			float bottomY = mesh.vertices[0].y;
+			float topY = mesh.vertices[0].y;
+			
+			for (int i = 1; i < count; i++) {
+				float y = mesh.vertices[i].y;
+				if (y > topY) {
+					topY = y;
+				}
+				else if (y < bottomY) {
+					bottomY = y;
+				}
+			}
+			
+			float uiElementHeight = topY - bottomY;
+			
+			Color32[] cs = mesh.colors32;
+			for (int i = 0; i < count; i++) {
+				Vector3 uiVertex = mesh.vertices[i];
+				cs[i] = Color32.Lerp(bottomColor, topColor, (uiVertex.y - bottomY) / uiElementHeight);
+			}
+			mesh.colors32 = cs ;
+		}
+	}
+
+
+#endif
 }
