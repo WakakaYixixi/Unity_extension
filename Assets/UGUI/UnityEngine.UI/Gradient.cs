84589918d1a0685e70853b0b78185f2c9edf2e4a
周张林 <zhouzhanglin@gmail.com>
2015-10-22 22:17:12 +0800

    Fixed UI Gradient

--- Assets/UGUI/UnityEngine.UI/Gradient.cs
+++ Assets/UGUI/UnityEngine.UI/Gradient.cs
@@ -14,7 +14,6 @@ namespace UnityEngine.UI
 		[SerializeField]
 		private Color32 bottomColor = Color.black;
 
-#if UNITY_5_2_1
 		public override void ModifyMesh (VertexHelper vh){
 			if (!IsActive()) {
 				return;
@@ -48,42 +47,4 @@ namespace UnityEngine.UI
 			}
 		}
 	}
-#elif UNITY_5_2
-
-	
-	public override void ModifyMesh (Mesh mesh)
-	{
-		if (!IsActive()) {
-			return;
-		}
-		
-		int count = mesh.vertexCount;
-		if(count>0){
-			
-			float bottomY = mesh.vertices[0].y;
-			float topY = mesh.vertices[0].y;
-			
-			for (int i = 1; i < count; i++) {
-				float y = mesh.vertices[i].y;
-				if (y > topY) {
-					topY = y;
-				}
-				else if (y < bottomY) {
-					bottomY = y;
-				}
-			}
-			
-			float uiElementHeight = topY - bottomY;
-			
-			Color32[] cs = mesh.colors32;
-			for (int i = 0; i < count; i++) {
-				Vector3 uiVertex = mesh.vertices[i];
-				cs[i] = Color32.Lerp(bottomColor, topColor, (uiVertex.y - bottomY) / uiElementHeight);
-			}
-			mesh.colors32 = cs ;
-		}
-	}
-
-
-#endif
 }
