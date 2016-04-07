f542d22987afd17d920e67355910bc83a4cd1e14
zhouzhanglin <zhouzhanglin@smalltreemedia.com>
2016-04-07 10:41:39 +0800

    通过设置图片来实现遮罩

---
+++ Assets/Native2D/Shader/SpriteMaskByTexture.shader
@@ -0,0 +1,107 @@
+﻿Shader "ZZL/Native2D/Sprite Mask By Texture"
+{
+	Properties
+	{
+		[PerRendererData] _MainTex ("Sprite Texture", 2D) = "white" {}
+		_Color ("Tint", Color) = (1,1,1,1)
+		[MaterialToggle] PixelSnap ("Pixel snap", Float) = 0
+        _StencilVal ("Stencil Ref",Int) = 1
+
+		_MaskTex("Mask Texture",2D)= "white" {}
+	}
+
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+
+			Stencil
+			{
+				Ref [_StencilVal]
+				Comp always
+				Pass replace
+			}
+
+		CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile _ PIXELSNAP_ON
+			#include "UnityCG.cginc"
+			
+			struct appdata_t
+			{
+				float4 vertex   : POSITION;
+				float4 color    : COLOR;
+				float2 texcoord : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+				fixed4 color    : COLOR;
+				half2 texcoord  : TEXCOORD0;
+				float4 worldPosition : TEXCOORD1;
+			};
+			
+			fixed4 _Color;
+			float4 _ClipRect;
+			half _Size = 1;
+
+			v2f vert(appdata_t IN)
+			{
+				v2f OUT;
+				OUT.worldPosition = mul(_Object2World,IN.vertex);
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.texcoord = IN.texcoord;
+				OUT.color = IN.color * _Color;
+				#ifdef PIXELSNAP_ON
+				OUT.vertex = UnityPixelSnap (OUT.vertex);
+				#endif
+
+				return OUT;
+			}
+
+			sampler2D _MainTex;
+			sampler2D _AlphaTex;
+			sampler2D _MaskTex;
+			float _AlphaSplitEnabled;
+
+			fixed4 SampleSpriteTexture (float2 uv)
+			{
+				fixed4 color = tex2D (_MainTex, uv);
+				if (_AlphaSplitEnabled)
+					color.a = tex2D (_AlphaTex, uv).r;
+
+				return color;
+			}
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+				fixed4 color = SampleSpriteTexture (IN.texcoord) * IN.color;
+				color.rgb *= color.a;
+
+				fixed4 mask = tex2D (_MaskTex,(IN.worldPosition.xy - _ClipRect.xy)/_ClipRect.zw );
+				color*=mask.a;
+
+				clip (color.a - 0.001);
+
+				return color;
+			}
+		ENDCG
+		}
+	}
+}
--- Assets/Native2D/SpriteMask/SpriteMask.cs
+++ Assets/Native2D/SpriteMask/SpriteMask.cs
@@ -8,6 +8,9 @@ public class SpriteMask : MonoBehaviour {
 	public Rect maskSize = new Rect(0,0,1,1);
 	public Material[] maskMaterials;
 
+	//如果是Texture 遮罩，小心贴图边界的处理,图片的边界最好有1-2像素是透明的,mask图片不要设置mipmap
+	public bool isTextureMask = false;
+
 	// Use this for initialization
 	void Start () {
 		Clip();
@@ -22,25 +25,46 @@ public class SpriteMask : MonoBehaviour {
 			Vector4 rect  = new Vector4(maskSize.x,maskSize.y,maskSize.width,maskSize.height);
 			rect.x+=transform.position.x;
 			rect.y+=transform.position.y;
-			rect.z+=transform.position.x+maskSize.x;
-			rect.w+=transform.position.y+maskSize.y;
+			if(isTextureMask){
+				rect.z+=maskSize.x;
+				rect.w+=maskSize.y;
+			}
+			else{
+				rect.z+=transform.position.x+maskSize.x;
+				rect.w+=transform.position.y+maskSize.y;
+			}
 			for(int i=0;i<maskMaterials.Length;++i){
-				maskMaterials[i].SetVector("_ClipRect",rect);
+				if(maskMaterials[i])
+					maskMaterials[i].SetVector("_ClipRect",rect);
 			}
 		}
 	}
 
 	void OnDrawGizmos(){
-		Gizmos.color = Color.red;
+		if(!isTextureMask){
+			Gizmos.color = Color.red;
 
-		Vector3 pos = new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
-		Vector3 pos1 = pos+new Vector3(maskSize.width,0,0);
-		Gizmos.DrawLine( pos,pos1);
-		pos = pos1+new Vector3(0,maskSize.height,0);
-		Gizmos.DrawLine( pos,pos1);
-		pos1 = pos-new Vector3(maskSize.width,0,0);
-		Gizmos.DrawLine( pos,pos1);
-		pos=new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
-		Gizmos.DrawLine( pos,pos1);
+			Vector3 pos = new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
+			Vector3 pos1 = pos+new Vector3(maskSize.width,0,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos = pos1+new Vector3(0,maskSize.height,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos1 = pos-new Vector3(maskSize.width,0,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos=new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
+			Gizmos.DrawLine( pos,pos1);
+		}else{
+			Gizmos.color = Color.green;
+
+			Vector3 pos = new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
+			Vector3 pos1 = pos+new Vector3(maskSize.width,0,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos = pos1+new Vector3(0,maskSize.height,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos1 = pos-new Vector3(maskSize.width,0,0);
+			Gizmos.DrawLine( pos,pos1);
+			pos=new Vector3(maskSize.x+transform.position.x,maskSize.y+transform.position.y,transform.position.z);
+			Gizmos.DrawLine( pos,pos1);
+		}
 	}
 }
