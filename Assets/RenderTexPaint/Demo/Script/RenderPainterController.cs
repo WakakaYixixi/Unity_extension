e0f4681ce6a62c4cb139ce4d4d4a9eb455b0bcdb
周张林 <zhouzhanglin@gmail.com>
2016-05-14 10:42:09 +0800

    no message

--- Assets/RenderTexPaint/Demo/Script/RenderPainterController.cs
+++ Assets/RenderTexPaint/Demo/Script/RenderPainterController.cs
@@ -6,9 +6,7 @@ using System.Collections;
 /// </summary>
 public class RenderPainterController : MonoBehaviour {
 
-	public RenderTexturePainterEx painterEx;
-	public RenderTexturePainter painter;
-//	public PaintCompleteChecker checker;
+	public RenderTexturePainter painterEx;
 	private bool m_isEraser = false;
 	private float m_alpha = 1f;
 	private bool m_clickDraw = false;
@@ -16,11 +14,12 @@ public class RenderPainterController : MonoBehaviour {
 	public Texture[] penTexs;
 	private int m_penTexIndex;
 
+	public PaintCompleteChecker checker;
+	public bool enableCheckComplete=false;
+
 	// Use this for initialization
 	void Start () {
 		Application.targetFrameRate=60;
-		if(painter) 
-			m_isEraser = painter.isEraser;
 		if(painterEx) 
 			m_isEraser = painterEx.isEraser;
 	}
@@ -33,60 +32,45 @@ public class RenderPainterController : MonoBehaviour {
 				painterEx.ClickDraw(Input.mousePosition);
 			}
 		}
-
 		if(Input.GetMouseButton(0)){
-			if(painter) 
-				painter.Drawing(Input.mousePosition);
 			if(painterEx) {
 				if(!m_clickDraw)
 					painterEx.Drawing(Input.mousePosition);
 			}
-//			checker.Drawing(Input.mousePosition);
+			if(enableCheckComplete && checker)
+				checker.Drawing(Input.mousePosition);
 		}
 		if(Input.GetMouseButtonUp(0)){
-			if(painter) 
-				painter.EndDraw();
 			if(painterEx) 
 				painterEx.EndDraw();
-//			checker.EndDraw();
+			if(enableCheckComplete && checker)
+				checker.EndDraw();
 		}
 	}
 
 
 	void OnGUI(){
-		if(painter){
-			if(GUI.Button(new Rect(10,10,100,30),"Clear")){
-				painter.ClearCanvas();
-			}
-			m_isEraser = GUI.Toggle(new Rect(120,10,100,30),m_isEraser,"Is Earse");
-			if(m_isEraser!=painter.isEraser){
-				painter.SetIsEraser(m_isEraser);
-			}
-			painter.brushScale = GUI.HorizontalSlider(new Rect(10, 80, 200, 30), painter.brushScale , 0.1F, 5F);
-
-		}
-		else if(painterEx)
+		if(painterEx)
 		{
-
-			if(GUI.Button(new Rect(10,10,100,30),"Clear")){
-				painterEx.ClearCanvas();
+			if(GUI.Button(new Rect(10,10,100,40),"Reset Canvas")){
+				painterEx.ResetCanvas();
 			}
-			m_isEraser = GUI.Toggle(new Rect(120,10,100,30),m_isEraser,"Is Earse","Button");
+			m_isEraser = GUI.Toggle(new Rect(120,10,100,40),m_isEraser,"Is Earse","Button");
 			if(m_isEraser!=painterEx.isEraser){
 				painterEx.SetIsEraser(m_isEraser);
 			}
-			m_clickDraw = GUI.Toggle(new Rect(240,10,100,30),m_clickDraw,"Click Draw","Button");
+			m_clickDraw = GUI.Toggle(new Rect(240,10,100,40),m_clickDraw,"Click Draw","Button");
 
 			GUI.color = Color.white;
-			GUI.Label( new Rect(10, 60, 200, 30) ,"Brush Scale :"+painterEx.brushScale.ToString("N2"));
-			painterEx.brushScale = GUI.HorizontalSlider(new Rect(10, 80, 200, 30), painterEx.brushScale , 0.1F, 5F);
+			GUI.Label( new Rect(10, 60, 200, 40) ,"Brush Scale :"+painterEx.brushScale.ToString("N2"));
+			painterEx.brushScale = GUI.HorizontalSlider(new Rect(10, 80, 200, 40), painterEx.brushScale , 0.1F, 5F);
 
 			GUI.color = Color.white;
-			GUI.Label( new Rect(10, 100, 200, 30) ,"Canvas Alpha :"+m_alpha.ToString("N2"));
-			m_alpha = GUI.HorizontalSlider(new Rect(10, 120, 200, 30), m_alpha , 0F, 1F);
+			GUI.Label( new Rect(10, 100, 200, 40) ,"Canvas Alpha :"+m_alpha.ToString("N2"));
+			m_alpha = GUI.HorizontalSlider(new Rect(10, 120, 200, 40), m_alpha , 0F, 1F);
 			painterEx.SetCanvasAlpha(m_alpha);
 
-			if(penTexs.Length>1 && GUI.Button(new Rect(220,60,150,30),"Change PenTexture")){
+			if(penTexs.Length>1 && GUI.Button(new Rect(220,60,150,40),"Change PenTexture")){
 				++m_penTexIndex;
 				if(m_penTexIndex>=penTexs.Length) m_penTexIndex = 0;
 				painterEx.penTex = penTexs[m_penTexIndex];
