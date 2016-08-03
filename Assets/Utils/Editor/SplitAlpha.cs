d3a1e63f1d327286a0939261949102ec990c12cd
周张林 <zhouzhanglin@gmail.com>
2016-08-03 20:12:04 +0800

    在生成RGB+A图片时，先获取之前生成的配置的pivot,border

--- Assets/Utils/Editor/SplitAlpha.cs
+++ Assets/Utils/Editor/SplitAlpha.cs
@@ -10,11 +10,11 @@ using System.Collections.Generic;
 /// </summary>
 public class SplitAlpha {
 
-	[MenuItem("Tools/Split Alpha/RGB+A可能会合并成一张图")]
+	[MenuItem("Tools/Split Alpha/RGB+A可能会合并成一张图",false,1)]
 	static void SplitUseMerge(){
 		Split(true);
 	}
-	[MenuItem("Tools/Split Alpha/RGB+A两张图")]
+	[MenuItem("Tools/Split Alpha/RGB+A两张图",false,1)]
 	static void SplitNonMerge(){
 		Split(false);
 	}
@@ -23,10 +23,10 @@ public class SplitAlpha {
 		if(Selection.activeObject && Selection.activeObject is Texture2D)
 		{
 			string name = (Selection.activeObject as Texture2D).name;
-			string folder = AssetDatabase.GetAssetPath(Selection.activeObject);
+			string file = AssetDatabase.GetAssetPath(Selection.activeObject);
 
-			List<SpriteMetaData> metaDatas = new List<SpriteMetaData>();
-			foreach(Object o in AssetDatabase.LoadAllAssetsAtPath(folder))
+			Dictionary<string,SpriteMetaData> metaDataKV = new Dictionary<string, SpriteMetaData>();
+			foreach(Object o in AssetDatabase.LoadAllAssetsAtPath(file))
 			{
 				if(o is Sprite)
 				{
@@ -36,11 +36,11 @@ public class SplitAlpha {
 					metaData.rect = s.rect;
 					metaData.pivot = s.pivot;
 					metaData.border = s.border;
-					metaDatas.Add(metaData);
+					metaDataKV[s.name] = metaData;
 				}
 			}
 
-			folder = Application.dataPath.Substring(0,Application.dataPath.LastIndexOf('/')+1) + folder.Substring(0,folder.LastIndexOf('/')+1);
+			string folder = Application.dataPath.Substring(0,Application.dataPath.LastIndexOf('/')+1) + file.Substring(0,file.LastIndexOf('/')+1);
 			Texture2D t = new Texture2D(1,1,TextureFormat.ARGB32,false,true);
 			t.filterMode = FilterMode.Trilinear;
 			t.LoadImage(File.ReadAllBytes(folder+name+".png"));
@@ -71,31 +71,41 @@ public class SplitAlpha {
 			string atlasPath = "";
 			if(merge && t.width>t.height)
 			{
+				atlasPath = folder+name+"_RGB_A.jpg";
+				LoadExistAltas(metaDataKV, atlasPath);
 				//两张图片合成一张
 				Texture2D atlasT = new Texture2D(t.width,t.width,TextureFormat.RGB24,false,true);
 				atlasT.SetPixels(0,0,t.width,t.height,jpg.GetPixels());
 				atlasT.SetPixels(0,t.height,t.width,t.height,alphaT.GetPixels());
-				File.WriteAllBytes(folder+name+"_RGB_A.jpg",atlasT.EncodeToJPG(100));
-				atlasPath = folder+name+"_RGB_A.jpg";
+				File.WriteAllBytes(atlasPath,atlasT.EncodeToJPG(100));
 			}
 			else if(merge && t.width<t.height)
 			{
+				atlasPath = folder+name+"_RGB_A.jpg";
+				LoadExistAltas(metaDataKV, atlasPath);
 				//两张图片合成一张
 				Texture2D atlasT = new Texture2D(t.height,t.height,TextureFormat.RGB24,false,true);
 				atlasT.SetPixels(0,0,t.width,t.height,jpg.GetPixels());
 				atlasT.SetPixels(t.width,0,t.width,t.height,alphaT.GetPixels());
 				File.WriteAllBytes(folder+name+"_RGB_A.jpg",atlasT.EncodeToJPG(100));
-				atlasPath = folder+name+"_RGB_A.jpg";
 
 			}else{
+
+				atlasPath = folder+name+"_RGB.jpg";
+				LoadExistAltas(metaDataKV, atlasPath);
+
 				byte[] alphaBytes = alphaT.EncodeToJPG(100);
 				File.WriteAllBytes(folder+name+"_A.jpg",alphaBytes);
 
 				byte[] jpgbytes = jpg.EncodeToJPG(100);
 				File.WriteAllBytes(folder+name+"_RGB.jpg",jpgbytes);
-				atlasPath = folder+name+"_RGB.jpg";
 			}
 			atlasPath = atlasPath.Substring(atlasPath.IndexOf("/Assets")+1);
+
+			List<SpriteMetaData> metaDatas = new List<SpriteMetaData>();
+			foreach(SpriteMetaData data in metaDataKV.Values){
+				metaDatas.Add(data);
+			}
 			if(metaDatas.Count>0){
 				AssetDatabase.Refresh();
 				SetSprite(metaDatas,atlasPath);
@@ -105,15 +115,62 @@ public class SplitAlpha {
 		}
 	}
 
+	static void LoadExistAltas(Dictionary<string,SpriteMetaData> metaDataKV ,string path){
+		path = path.Substring(path.IndexOf("Assets/"));
+		if(File.Exists(path)){
+			foreach(Object o in AssetDatabase.LoadAllAssetsAtPath(path))
+			{
+				if(o is Sprite)
+				{
+					Sprite s = o as Sprite;
+					if(metaDataKV.ContainsKey(s.name)){
+						SpriteMetaData metaData = new SpriteMetaData();
+						Vector2 pivot = new Vector2(s.pivot.x/s.rect.width,s.pivot.y/s.rect.height);
+						metaData.border = s.border;
+						metaData.name = s.name;
+						metaData.rect = s.rect;
+						metaData.alignment=(int)GetSpriteAlignment(pivot);
+						if(metaData.alignment==(int)SpriteAlignment.Custom){
+							metaData.pivot = pivot;
+						}
+						metaDataKV[s.name] = metaData;
+					}
+				}
+			}
+		}
+	}
+
+	static SpriteAlignment GetSpriteAlignment(Vector2 pivot){
+		if(pivot.x==0.5f && pivot.y==0.5f){
+			return SpriteAlignment.Center;
+		}else if(pivot.x==0f && pivot.y==1f){
+			return SpriteAlignment.TopLeft;
+		}else if(pivot.x==1f && pivot.y==1f){
+			return SpriteAlignment.TopRight;
+		}else if(pivot.x==0.5f && pivot.y==1f){
+			return SpriteAlignment.TopCenter;
+		}else if(pivot.x==0f && pivot.y==0){
+			return SpriteAlignment.BottomLeft;
+		}else if(pivot.x==1f && pivot.y==0f){
+			return SpriteAlignment.BottomRight;
+		}else if(pivot.x==0.5f && pivot.y==0f){
+			return SpriteAlignment.BottomCenter;
+		}else if(pivot.x==0f && pivot.y==0.5f){
+			return SpriteAlignment.LeftCenter;
+		}else if(pivot.x==1f && pivot.y==0.5f){
+			return SpriteAlignment.RightCenter;
+		}
+		return SpriteAlignment.Custom;
+	}
+
 	static void SetSprite(List<SpriteMetaData> metaDatas,string atlasPath){
 		TextureImporter textureImporter = AssetImporter.GetAtPath(atlasPath) as TextureImporter;
 		textureImporter.maxTextureSize = 2048;
-		textureImporter.spritesheet = metaDatas.ToArray();
 		textureImporter.textureType = TextureImporterType.Sprite;
 		textureImporter.spriteImportMode = SpriteImportMode.Multiple;
-		textureImporter.spritePivot = new Vector2(0.5f, 0.5f);
 		textureImporter.spritePixelsPerUnit = 100;
 		textureImporter.mipmapEnabled=false;
+		textureImporter.spritesheet = metaDatas.ToArray();
 		AssetDatabase.ImportAsset(atlasPath, ImportAssetOptions.ForceUpdate);
 	}
 }
