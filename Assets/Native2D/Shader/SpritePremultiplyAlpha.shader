48b2a61dbe606080e9dc94e10e7df37c5b2b7ea6
zhouzhanglin <zhouzhanglin@smalltreemedia.com>
2017-01-05 15:36:23 +0800

    no message

---
+++ Assets/Native2D/Shader/SpritePremultiplyAlpha.shader
@@ -0,0 +1,71 @@
+﻿//author:zhouzhanglin
+//预乘Alpha
+Shader "ZZL/Native2D/Sprite Premultiply Alpha"
+{
+	Properties
+	{
+		[HideInInspector]_MainTex ("Texture", 2D) = "white" {}
+		_Color ("Tint", Color) = (1,1,1,1)
+		[Enum(UnityEngine.Rendering.CullMode)]_CullMode("Cull Mode",float)=0
+	}
+	SubShader
+	{
+		Tags
+		{ 
+			"Queue"="Transparent" 
+			"IgnoreProjector"="True" 
+			"RenderType"="Transparent" 
+			"PreviewType"="Plane"
+			"CanUseSpriteAtlas"="True"
+		}
+	
+		Lighting off
+		Zwrite off
+		Cull [_CullMode]
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				fixed4 color:COLOR;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				fixed4 color:COLOR;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+
+			fixed4 _Color;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.uv;
+				o.color = v.color*_Color;
+				return o;
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D (_MainTex, i.uv)*i.color;
+				col*=col.a;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Utils/Editor/PngProcess.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+/// <summary>
+/// png处理
+/// Author:zhouzhanglin
+/// </summary>
+public class PngProcess : Editor {
+
+	[MenuItem("Tools/Premulity Alpha(文件夹)",false,64)]
+	static void PremulityAlphaFolder () {
+
+		if(Selection.activeObject is DefaultAsset)
+		{
+			Dictionary<string,string> texturePathKV = new Dictionary<string, string>();
+			string dirPath = AssetDatabase.GetAssetOrScenePath(Selection.activeObject);
+			if(Directory.Exists(dirPath)){
+				foreach (string path in Directory.GetFiles(dirPath))
+				{  
+					if(path.LastIndexOf(".meta")==-1){
+						if( System.IO.Path.GetExtension(path) == ".png"){
+							int start = path.LastIndexOf("/")+1;
+							int end = path.LastIndexOf(".png");
+							texturePathKV[path.Substring(start,end-start)] = path.Substring(6);
+							continue;
+						}
+					}
+				} 
+
+
+				if( texturePathKV.Count>0){
+					foreach(string fileName in texturePathKV.Keys){
+						string path = texturePathKV[fileName];
+						Texture2D t = LoadPNG(Application.dataPath+"/"+path);
+						Color32[] colors =  t.GetPixels32();
+						for(int i=0;i<colors.Length;++i){
+							Color32 c = colors[i];
+							c.r =(byte)( (c.a*c.r)/255);
+							c.g =(byte)( (c.a*c.g)/255);
+							c.b =(byte)( (c.a*c.b)/255);
+							colors[i] = c;
+						}
+						t.SetPixels32(colors);
+						byte[] bytes = t.EncodeToPNG();
+						SavePNG(Application.dataPath+fileName+".png",bytes);
+					}
+
+				}
+			}
+		}
+		AssetDatabase.Refresh();
+	}
+
+	[MenuItem("Tools/Premulity Alpha(图片)",false,65)]
+	static void PremulityAlphaFile(){
+		if(Selection.activeObject is DefaultAsset || Selection.activeObject is Texture2D)
+		{
+			string dirPath = AssetDatabase.GetAssetOrScenePath(Selection.activeObject);
+			if(File.Exists(dirPath) && dirPath.LastIndexOf(".meta")==-1 && System.IO.Path.GetExtension(dirPath) == ".png"){
+				int start = dirPath.LastIndexOf("/")+1;
+				int end = dirPath.LastIndexOf(".png");
+				string path = dirPath.Substring(6);
+
+				Texture2D t = LoadPNG(Application.dataPath+path);
+				Color32[] colors =  t.GetPixels32();
+				for(int i=0;i<colors.Length;++i){
+					Color32 c = colors[i];
+					c.r =(byte)( (c.a*c.r)/255);
+					c.g =(byte)( (c.a*c.g)/255);
+					c.b =(byte)( (c.a*c.b)/255);
+					colors[i] = c;
+				}
+				t.SetPixels32(colors);
+				byte[] bytes = t.EncodeToPNG();
+				SavePNG(Application.dataPath+path,bytes);
+				AssetDatabase.Refresh();
+			}
+		}
+	}
+
+	static Texture2D LoadPNG(string filePath) {
+
+		Texture2D tex = null;
+		byte[] fileData;
+
+		if (File.Exists(filePath))     {
+			fileData = File.ReadAllBytes(filePath);
+			tex = new Texture2D(2, 2);
+			tex.LoadImage(fileData);
+		}
+		return tex;
+	}
+
+	static void SavePNG(string filePath,byte[] fileData) {
+		File.WriteAllBytes(filePath,fileData);
+	}
+}
